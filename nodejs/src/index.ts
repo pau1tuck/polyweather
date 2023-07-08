7ab969e860305f1c6556e17902724b70cf6ee72b
Paul Tuck <paul.tuck@oxon.org>
2023-07-08 17:54:39 +0700

    "2023-07-08_17:54+07"

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,22 +1,18 @@
-import fetch, { Response } from "node-fetch";
+import axios from "axios";
 
-const API_KEY = process.env.API_KEY || null;
-
-const COUNTRY = process.env.COUNTRY || "England";
-const CITY = process.env.CITY || "London";
-const LATITUDE = process.env.LATITUDE || "51.5085";
-const LONGITUDE = process.env.LONGITUDE || "0.1257";
-
-process.stdout.write("Hello, World!");
-let weather_data = null;
-
-const fetch_weather_data = () => {
-    fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${LATITUDE}&longitude=${LONGITUDE}&hourly=temperature_2m,relativehumidity_2m,rain,weathercode,cloudcover,visibility,windspeed_180m,is_day&daily=sunrise,sunset&current_weather=true&timezone=Asia%2FBangkok`
-    )
-        .then((response: Response) => response.json())
-        .then((data: any) => console.log(data))
-        .catch((err: Error) => console.log("Unable to retrieve weather data from server:", err));
+const options = {
+    method: "GET",
+    url: "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather",
+    params: { city: "Seattle" },
+    headers: {
+        "X-RapidAPI-Key": "ef6b437320msha45a8942110aac3p10b0abjsnf5d68535cfba",
+        "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    },
 };
 
-fetch_weather_data();
+try {
+    const response = await axios.request(options);
+    console.log(response.data);
+} catch (error) {
+    console.error(error);
+}
