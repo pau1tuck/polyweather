4b4f28332e4f39efc8078124422aafc6560f1d96
Paul Tuck <paul.tuck@oxon.org>
2023-07-08 18:36:10 +0700

    "2023-07-08_18:36+07"

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,16 +1,22 @@
 import axios from "axios";
+import dotenv from "dotenv";
 import fs from "fs";
+
+dotenv.config();
+const city = process.env.CITY || "London";
+const state = process.env.STATE || "";
+const country = process.env.COUNTRY || "England";
 
 const options = {
     method: "GET",
     url: "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather",
     params: {
-        city: process.env.CITY || "London",
-        state: process.env.STATE || "",
-        country: process.env.COUNTRY || "England",
+        city,
+        state,
+        country,
     },
     headers: {
-        "X-RapidAPI-Key": "",
+        "X-RapidAPI-Key": process.env.API_KEY || null,
         "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
     },
 };
@@ -27,6 +33,9 @@ const get_weather_data = async () => {
         const response = await axios.request(options);
         console.log("Response status:", response.status);
         const data = response.data;
+        data.city = city;
+        data.state = state;
+        data.country = country;
         console.log(data);
         const jsonData = JSON.stringify(data);
         fs.writeFile("weather.json", jsonData, err => {
