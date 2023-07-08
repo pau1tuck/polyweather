21fe0892626537b359e50ed3355a890b56acbee3
Paul Tuck <paul.tuck@oxon.org>
2023-07-08 18:15:12 +0700

    "2023-07-08_18:15+07"

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -9,23 +9,29 @@ const options = {
         country: process.env.COUNTRY || "England",
     },
     headers: {
-        "X-RapidAPI-Key": process.env.API_KEY || null,
+        "X-RapidAPI-Key": "ef6b437320msha45a8942110aac3p10b0abjsnf5d68535cfba",
         "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
     },
 };
 
+// Validate Rapid API key
 if (options.headers["X-RapidAPI-Key"] === null) {
-    throw new Error("Invalid API key. Register at https://rapidapi.com/apininjas/api/weather-by-api-ninjas/");
+    console.log("Invalid API key. Register at https://rapidapi.com/apininjas/api/weather-by-api-ninjas/");
+    process.exit();
 }
 
 let weather_data = null;
 
 const get_weather_data = async () => {
+    console.log("Loading...");
     try {
         const response = await axios.request(options);
         console.log("Response status:", response.status);
         weather_data = response.data;
+        console.log(weather_data);
     } catch (error) {
         console.error(error);
     }
 };
+
+get_weather_data();
